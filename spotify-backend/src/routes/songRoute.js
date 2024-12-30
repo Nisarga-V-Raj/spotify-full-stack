420e30c1f7c5f8d02728eb5e272a2c3ef201e9b8
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2024-12-30 21:35:40 +0530

    fifth commit

--- spotify-backend/src/routes/songRoute.js
+++ spotify-backend/src/routes/songRoute.js
@@ -1,9 +1,10 @@
 import { addSong, listSong } from "../controllers/songController.js";
 import express from 'express'
+import uplaod from "../middleware/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add', addSong);
+songRouter.post('/add', uplaod.fields([{name: 'image', maxCount: 1}, {name: 'audio', maxCount: 1}]), addSong);
 songRouter.get('/list', listSong);
 
 export default songRouter;
