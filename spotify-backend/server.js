40e769c76e2eee509c7ac58dd11b33a6dd782924
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2024-12-30 19:48:07 +0530

    second commit

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import songRouter from './src/routes/songRoute.js';
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // initializing routes
+app.use("/api/song", songRouter)
 app.get('/', (req, res)=> res.send("API Working"))
 
 app.listen(port, ()=>console.log(`Server started on ${port}`));
